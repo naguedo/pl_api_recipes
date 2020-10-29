5d0781137190ee3ea45b0329f8528a44c769f640
Damien <damien.brincourt@qoqa.com>
2020-10-29 18:06:18 +0100

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001/', 'http://localhost:3000/', 'https://pl-recipes.herokuapp.com/'
+    origins 'localhost:3001/', 'localhost:3000/', 'https://pl-recipes.herokuapp.com/', 'https://blooming-taiga-65522.herokuapp.com/'
 
     resource '*',
       headers: :any,
